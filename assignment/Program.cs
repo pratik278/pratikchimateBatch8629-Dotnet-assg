dc281947b12be2f5f466d055b6c36f9b6bfade2e
pratik278 <pratikchimate52@gmail.com>
2021-12-01 23:20:14 +0530

    first commit

---
+++ assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_11
+{
+    class Class1
+    {
+        public static void Main(string[]args)
+        {
+
+            int x, y;
+            Console.WriteLine("Enter 1st Number");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 2nd Number");
+            y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine((x < 0 && y > 0) || (x > 0 && y < 0));
+            Console.ReadLine();
+
+
+        }
+    }
+}
